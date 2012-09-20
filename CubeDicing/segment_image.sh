9dfe0c2d137b7c15afba85b611524a8a6c78db3c
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-09-20 10:14:07 -0400

    changes for odyssey

--- CubeDicing/segment_image.sh
+++ CubeDicing/segment_image.sh
@@ -1,3 +1,3 @@
 DIR=$(dirname ${BASH_SOURCE[0]})
 SLICE_FILE=$(sed "${2}q;d" $1) 
-MATLABPATH="$DIR" /Applications/MATLAB_R2012a.app/bin/matlab -r -nojvm -nodesktop -nosplash -r "segment_image('$SLICE_FILE', '${DIR}/forest_TS1_TS3.mat','$3'); quit"
+MATLABPATH="$DIR" matlab -r -nojvm -nodesktop -nosplash -r "segment_image('$SLICE_FILE', '${DIR}/forest_TS1_TS3.mat','$3'); quit"
