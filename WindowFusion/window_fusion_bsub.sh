d007fb174a7b40427b6793cff8d10fa740cd98ed
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-10-03 15:26:58 -0400

    add LSF version of window_fusion script

--- WindowFusion/window_fusion_bsub.sh
+++ WindowFusion/window_fusion_bsub.sh
@@ -2,7 +2,7 @@ echo "Window Fusion" $*
 DIR=$(dirname ${BASH_SOURCE[0]})
 ARGS=$(printf ",'%s'" $* | cut -c2-)
 export MATLABPATH="${MATLABPATH}:${DIR}"
-matlab -nojvm -nodesktop -nosplash -r "window_fusion($ARGS)"
+bsub -K -q short_serial matlab -nojvm -nodesktop -nosplash -r "window_fusion($ARGS)"
 if [ -f "${@: -1}" ]
 then
     exit 0
