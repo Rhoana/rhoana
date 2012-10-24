24e8c405de2faac3e61ed1c7c3722a3e78d17dff
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-10-24 16:50:28 -0400

    memory requirements

--- CubeDicing/segment_image_bsub.sh
+++ CubeDicing/segment_image_bsub.sh
@@ -11,7 +11,7 @@ DIR=$(dirname ${BASH_SOURCE[0]})
 # move to 1-based line indexing
 IDX=$(expr $2 + 1)
 SLICE_FILE=$(sed "${IDX}q;d" $1) 
-MATLABPATH="$DIR" bsub -K -q short_serial matlab -nojvm -nodesktop -nosplash -r "segment_image('$SLICE_FILE', '${DIR}/forest_TS1_TS3.mat','$3'); quit"
+MATLABPATH="$DIR" bsub -R "rusage[mem=5000]" -K -q normal_serial matlab -nojvm -nodesktop -nosplash -r "segment_image('$SLICE_FILE', '${DIR}/forest_TS1_TS3.mat','$3'); quit"
 if [ -f "$3" ]
 then
     exit 0
