21a857f59dd594d96af6d628925148fdb8a5268d
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-10-25 11:00:57 -0400

    report matlab errors

--- CubeDicing/dice_block_bsub.sh
+++ CubeDicing/dice_block_bsub.sh
@@ -1,7 +1,7 @@
 echo "Dicing" $*
 DIR=$(dirname ${BASH_SOURCE[0]})
 ARGS=$(printf ",'%s'" $* | cut -c2-)
-MATLABPATH="$DIR" bsub -K -q short_serial matlab -r -nojvm -nodesktop -nosplash -r "try, dice_block($ARGS), catch, exit(1), end, exit(0)"
+MATLABPATH="$DIR" bsub -K -q short_serial matlab -r -nojvm -nodesktop -nosplash -r "try, dice_block($ARGS), catch err, display(getReport(err, 'extended')), exit(1), end, exit(0)"
 if [ -f "${@: -1}" ]
 then
     exit 0
