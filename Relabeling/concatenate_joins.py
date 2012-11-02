9f897d1e3bf8cd0820e28315f657064cedb37f72
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-11-02 11:02:07 -0400

    python script to contatenate joins from multiple pairwise matches

---
+++ Relabeling/concatenate_joins.py
@@ -0,0 +1,19 @@
+import sys
+import h5py
+import numpy as np
+import shutil
+
+if __name__ == '__main__':
+    outf = h5py.File(sys.argv[-1] + '_partial', 'w')
+
+    outjoins = np.zeros((2, 0), dtype=np.uint64)
+    for filename in sys.argv[:-1]:
+        f = h5py.File(filename)
+        if 'joins' in f:
+            outjoins = np.hstack((outjoins, f['joins'][...]))
+
+    ds = outf.create_dataset('joins', outjoins.shape, outjoins.dtype)
+    ds[...] = outjoins
+    outf.close()
+
+    shutil.move(sys.argv[-1] + '_partial', sys.argv[-1])
