7a75087fc823605556aa208c09cc5e91357d2f88
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-11-02 11:10:04 -0400

    off-by-one error, and noisier output

--- Relabeling/concatenate_joins.py
+++ Relabeling/concatenate_joins.py
@@ -7,10 +7,13 @@ if __name__ == '__main__':
     outf = h5py.File(sys.argv[-1] + '_partial', 'w')
 
     outjoins = np.zeros((2, 0), dtype=np.uint64)
-    for filename in sys.argv[:-1]:
+    for filename in sys.argv[1:-1]:
         f = h5py.File(filename)
         if 'joins' in f:
+            print filename, f['joins'][...]
             outjoins = np.hstack((outjoins, f['joins'][...]))
+        else:
+            print "no joins in", filename
 
     ds = outf.create_dataset('joins', outjoins.shape, outjoins.dtype)
     ds[...] = outjoins
