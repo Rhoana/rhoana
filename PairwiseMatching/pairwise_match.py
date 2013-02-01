3ee670c47804b9109303cc1df6bc049fcd8b307f
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-01-31 21:45:31 -0500

    rewrite pairwise matching to run joins each step

--- PairwiseMatching/pairwise_match.py
+++ PairwiseMatching/pairwise_match.py
@@ -42,11 +42,20 @@ minoverlap_single_ratio = 0.9; # If either overlap is above this then join
 print 'Running pairwise matching', " ".join(sys.argv[1:])
 
 # Extract overlapping regions
-bl1f = h5py.File(block1_path)
+bl1f = h5py.File(block1_path, 'r')
 block1 = bl1f['labels']
-bl2f = h5py.File(block2_path)
+bl2f = h5py.File(block2_path, 'r')
 block2 = bl2f['labels']
 assert block1.size == block2.size
+
+# append the blocks, and pack them so we can use the fast 64-bit counter
+stacked = np.vstack((block1, block2))
+inverse, packed = np.unique(stacked, return_inverse=True)
+packed = packed.reshape(stacked.shape)
+packed_block1 = packed[:block1.shape[0], :, :]
+packed_block2 = packed[block1.shape[0]:, :, :]
+
+# extract overlap
 
 lo_block1 = [0, 0, 0];
 hi_block1 = [None, None, None]
@@ -60,44 +69,36 @@ direction = 3 - direction
 lo_block1[direction] = - 2 * halo_size
 hi_block2[direction] = 2 * halo_size;
 
-# extract overlap
 block1_slice = tuple(slice(l, h) for l, h in zip(lo_block1, hi_block1))
 block2_slice = tuple(slice(l, h) for l, h in zip(lo_block2, hi_block2))
-block1_overlap = block1[block1_slice]
-block2_overlap = block2[block2_slice]
-
-if Debug:
-    import pylab
-    pylab.imshow(block1[0, :, :])
-    pylab.title('block1')
-    pylab.figure()
-    pylab.imshow(block1_overlap[0, :, :])
-    pylab.title('block1 over')
-    pylab.figure()
-    pylab.imshow(block2[0, :, :])
-    pylab.title('block2')
-    pylab.figure()
-    pylab.imshow(block2_overlap[0, :, :])
-    pylab.title('block2 over')
-    pylab.figure()
-
-    pylab.show()
-
-
-# append the blocks, and pack them so we can use the fast 64-bit counter
-stacked = np.vstack((block1_overlap, block2_overlap))
-inverse, packed = np.unique(stacked, return_inverse=True)
-packed = packed.reshape(stacked.shape)
-packed1 = packed[:block1_overlap.shape[0], :, :]
-packed2 = packed[block1_overlap.shape[0]:, :, :]
+packed_overlap1 = packed_block1[block1_slice]
+packed_overlap2 = packed_block2[block2_slice]
 
 counter = fast64counter.ValueCountInt64()
-counter.add_values_pair32(packed1.astype(np.int32).ravel(), packed2.astype(np.int32).ravel())
+counter.add_values_pair32(packed_overlap1.astype(np.int32).ravel(), packed_overlap2.astype(np.int32).ravel())
 overlap_labels1, overlap_labels2, overlap_areas = counter.get_counts_pair32()
 
 areacounter = fast64counter.ValueCountInt64()
-areacounter.add_values(packed.ravel())
+areacounter.add_values(packed_overlap1.ravel())
+areacounter.add_values(packed_overlap2.ravel())
 areas = dict(zip(*areacounter.get_counts()))
+
+if Debug:
+    import pylab
+    pylab.figure()
+    pylab.imshow(block1[0, :, :] % 13)
+    pylab.title('block1')
+    pylab.figure()
+    pylab.imshow(block2[0, :, :] % 13)
+    pylab.title('block2')
+    pylab.figure()
+    pylab.imshow(packed_overlap1[0, :, :] % 13)
+    pylab.title('packed overlap1')
+    pylab.figure()
+    pylab.imshow(packed_overlap2[0, :, :] % 13)
+    pylab.title('packed overlap2')
+
+    pylab.show()
 
 to_merge = []
 to_steal = []
@@ -110,109 +111,73 @@ for l1, l2, overlap_area in zip(overlap_labels1, overlap_labels2, overlap_areas)
           (overlap_area > minoverlap_single_ratio * areas[l2]) or
           ((overlap_area > minoverlap_dual_ratio * areas[l1]) and
            (overlap_area > minoverlap_dual_ratio * areas[l2]))))):
-        to_merge.append((inverse[l1], inverse[l2]))
+        if inverse[l1] != inverse[l2]:
+            to_merge.append((inverse[l1], inverse[l2]))
     else:
         to_steal.append((overlap_area, l1, l2))
 
 # Merges are handled later
 
-if Debug:
-    import pylab
-    pylab.figure()
-    pylab.imshow(packed1[0, :, :] % 13)
-    pylab.title('packed1before')
-    pylab.figure()
-    pylab.imshow(packed2[0, :, :] % 13)
-    pylab.title('packed2before')
-
-
 # Process steals
-packed1_face = packed1[tuple(0 if i == direction else slice(None, None) for i in range (3))]
-packed2_face = packed2[tuple(-1 if i == direction else slice(None, None) for i in range (3))]
+packed_overlap1_face = packed_overlap1[tuple(0 if i == direction else slice(None, None) for i in range (3))]
+packed_overlap2_face = packed_overlap2[tuple(-1 if i == direction else slice(None, None) for i in range (3))]
 
 faceareacounter = fast64counter.ValueCountInt64()
-faceareacounter.add_values(packed1_face.ravel())
-faceareacounter.add_values(packed2_face.ravel())
+faceareacounter.add_values(packed_overlap1_face.ravel())
+faceareacounter.add_values(packed_overlap2_face.ravel())
 face_areas = defaultdict(int)
 face_areas.update(dict(zip(*faceareacounter.get_counts())))
 for _, l1, l2 in reversed(sorted(to_steal)):  # work largest to smallest
     if face_areas[l1] >= face_areas[l2]:
-        packed2[(packed1 == l1)] = l1
+        packed_overlap2[(packed_overlap1 == l1)] = l1
     else:
-        packed1[(packed2 == l2)] = l2
+        packed_overlap1[(packed_overlap2 == l2)] = l2
 
 if Debug:
     import pylab
     pylab.figure()
-    pylab.imshow(packed1[0, :, :] % 13)
-    pylab.title('packed1')
+    pylab.imshow(packed_overlap1[0, :, :] % 13)
+    pylab.title('packed_overlap1 post steal')
     pylab.figure()
-    pylab.imshow(packed2[0, :, :] % 13)
-    pylab.title('packed2')
+    pylab.imshow(packed_overlap2[0, :, :] % 13)
+    pylab.title('packed_overlap2 post steal')
     pylab.show()
 
+# handle merges by rewriting the inverse
+merge_map = dict(reversed(sorted(s)) for s in to_merge)
+for idx, val in enumerate(inverse):
+    if val in merge_map:
+        while val in merge_map:
+            val = merge_map[val]
+        inverse[idx] = val
 
 # Remap and merge
-block1[block1_slice] = inverse[packed1]
-block2[block2_slice] = inverse[packed2]
-
-if Debug:
-    import pylab
-    pylab.imshow(block1[0, :, :] % 13)
-    pylab.title('block1')
-    pylab.figure()
-    pylab.imshow(block2[0, :, :] % 13)
-    pylab.title('block2')
-    pylab.show()
-
-
-if Debug:
-    # this is slow
-    block1data = block1[...]
-    block2data = block2[...]
-    for l1, l2 in to_merge:
-        block1data[block1data == l1] = l2
-        block2data[block2data == l1] = l2
-
-    import pylab
-    pylab.imshow(block1data[0, :, :] % 13)
-    pylab.title('block1')
-    pylab.figure()
-    pylab.imshow(block2data[0, :, :] % 13)
-    pylab.title('block2')
-    pylab.show()
-
 out1 = h5py.File(outblock1_path + '_partial', 'w')
 out2 = h5py.File(outblock2_path + '_partial', 'w')
-
 outblock1 = out1.create_dataset('/labels', block1.shape, block1.dtype, chunks=block1.chunks)
 outblock2 = out2.create_dataset('/labels', block2.shape, block2.dtype, chunks=block2.chunks)
-outblock1[...] = block1[...]
-outblock2[...] = block2[...]
+outblock1[...] = inverse[packed_block1]
+outblock2[...] = inverse[packed_block2]
 
-to_merge = np.array(to_merge).reshape((-1, 2))
-
-if 'joins' in bl1f:
-    joins = np.vstack((bl1f['joins'][...], to_merge))
-else:
-    joins = to_merge
-if joins.size > 0:
-    j = out1.create_dataset('/joins', joins.shape, np.uint64)
-    j[...] = joins
-
-if 'joins' in bl2f:
-    joins = bl2f['joins']
-    j = out2.create_dataset('/joins', joins.shape, np.uint64)
-    j[...] = joins[...]
+if Debug:
+    import pylab
+    pylab.figure()
+    pylab.imshow(outblock1[0, :, :] % 13)
+    pylab.title('final block1')
+    pylab.figure()
+    pylab.imshow(outblock2[0, :, :] % 13)
+    pylab.title('final block2')
+    pylab.show()
 
 # move to final location
+out1.close()
+out2.close()
+
 if os.path.exists(outblock1_path):
         os.unlink(outblock1_path)
 if os.path.exists(outblock2_path):
         os.unlink(outblock2_path)
 
-out1.close()
-out2.close()
 os.rename(outblock1_path + '_partial', outblock1_path)
 os.rename(outblock2_path + '_partial', outblock2_path)
 print "Successfully wrote", outblock1_path, 'and', outblock2_path
