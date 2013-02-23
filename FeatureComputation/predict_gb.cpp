faa6cb70604458794a4e5f03616741197b082f56
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-02-23 16:03:47 -0500

    compute pixel probabilities directly, rather than via opencv

--- FeatureComputation/predict_gb.cpp
+++ FeatureComputation/predict_gb.cpp
@@ -34,6 +34,7 @@ int main(int argc, char** argv)
     cv::FileStorage fs(argv[1], cv::FileStorage::READ);
     CvBoost classifier;
     classifier.read(*fs, *fs["classifier"]);
+    CvSeq* weak_classifiers = classifier.get_weak_predictors();
 
     // Fetch features
     H5File h5f = open_feature_file(argv[2]);
@@ -49,7 +50,7 @@ int main(int argc, char** argv)
     int col_block_size = imsize.width / (imsize.width / 1024) + 1;
 
     // Output image
-    Mat prediction(imsize, CV_32FC1);
+    Mat prediction = Mat::zeros(imsize, CV_32FC1);
     Rect fullrect(0, 0, imsize.width, imsize.height);
 
     for (int basecol = 0; basecol < imsize.width; basecol += col_block_size) {
@@ -58,25 +59,50 @@ int main(int argc, char** argv)
             Rect roi(basecol, baserow, col_block_size, row_block_size);
             roi &= fullrect;
 
-            // Stack columns
-            Mat stacked_features(roi.width * roi.height, num_features, CV_32F);
+            vector<Mat> features(num_features);
+            // Read features
             for (int fnum = 0; fnum < num_features; fnum++) {
-                Mat feature;
-                Mat dest;
-                read_feature(h5f, feature, names[fnum].c_str(), roi);
-                feature.reshape(0, roi.width * roi.height).copyTo(stacked_features.col(fnum));
+                read_feature(h5f, features[fnum], names[fnum].c_str(), roi);
             }
-            
-            Mat submat = prediction(roi);
-            int stacked_row_offset = 0;
-            for (int outrow = 0; outrow < roi.height; outrow++) {
-                float *dest = submat.ptr<float>(outrow);
-                for (int outcol = 0; outcol < roi.width; outcol++, stacked_row_offset++, dest++) {
-                    float sum = classifier.predict(stacked_features.row(stacked_row_offset), Mat(), Range::all(), false, true);
-                    // cout << sum << " " << 1 / (1 + exp(-sum)) << endl;
-                    *dest = 1 / (1 + exp(-sum));
+
+            // Predict
+            Mat GB_sum(features[0].size(), CV_32F);
+            int roi_size = roi.width * roi.height;
+
+            CvSeqReader reader;
+            cvStartReadSeq( weak_classifiers, &reader );
+            cvSetSeqReaderPos( &reader, 0);
+
+            for (int wi = 0; wi < weak_classifiers->total; wi++) {
+                CvBoostTree* bt;
+                CV_READ_SEQ_ELEM( bt, reader );
+                const CvDTreeNode *root = bt->get_root();
+                CvDTreeSplit* split = root->split;
+                int fnum = split->var_idx;
+                float thresh = split->ord.c;
+                float left_val = root->left->value;
+                float right_val = root->right->value;
+                if (split->inversed) {
+                    float temp = left_val;
+                    left_val = right_val;
+                    right_val = temp;
                 }
+                Mat result(features[0].size(), CV_32F);
+
+                // features and GB_sum are continuous
+                float *feature_ptr = features[fnum].ptr<float>(0);
+                float *score_ptr = GB_sum.ptr<float>(0);
+
+
+                for (int i = 0; i < roi_size; i++, feature_ptr++, score_ptr++)
+                    *score_ptr += ((*feature_ptr <= thresh) ? left_val : right_val);
             }
+            // convert to probability
+            float *score_ptr = GB_sum.ptr<float>(0);
+            for (int i = 0; i < roi_size; i++, score_ptr++)
+                *score_ptr = 1 / (1 + exp(- *score_ptr));
+
+            GB_sum.copyTo(prediction(roi));
         }
     }
 
@@ -85,11 +111,14 @@ int main(int argc, char** argv)
         imshow("result", prediction);
         waitKey(0);
     } else {
+        // Write probabilities, original, and all membrane features for next step
         H5File h5fout = create_feature_file(argv[3], prediction);
         write_feature(h5fout, prediction, "probabilities");
+        Mat feature;
+        read_feature(h5f, feature, "original");
+        write_feature(h5fout, feature, "original");
         for (int fnum = 0; fnum < num_features; fnum++) {
             if (names[fnum].find("membrane") != string::npos) {
-                Mat feature;
                 read_feature(h5f, feature, names[fnum].c_str());
                 write_feature(h5fout, feature, names[fnum].c_str());
             }
