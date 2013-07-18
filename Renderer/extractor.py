b0dae5d8a4cabbbb84b86466909583915583a4a2
Daniel Miron <dmmiron@gmail.com>
2013-07-18 12:28:05 -0400

    removed all unnecessary prints

--- Renderer/extractor.py
+++ Renderer/extractor.py
@@ -74,7 +74,6 @@ class Extractor:
         
     def run(self):
         for label_set in self.label_ids:
-            print label_set
             color = self.color_map[label_set[0] % len(self.color_map)]
             #color = self.color_map[1]
             for z in self.z_order:
